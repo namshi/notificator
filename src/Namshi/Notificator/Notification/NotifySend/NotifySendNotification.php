e6d111f07c2855dac98c3ee9c1c0e2e29ef1a1d6
odino <alessandro.nadalin@gmail.com>
2013-07-10 23:26:39 +0400

    added missing phpdoc

--- src/Namshi/Notificator/Notification/NotifySend/NotifySendNotification.php
+++ src/Namshi/Notificator/Notification/NotifySend/NotifySendNotification.php
@@ -4,10 +4,19 @@ namespace Namshi\Notificator\Notification\NotifySend;
 
 use Namshi\Notificator\Notification;
 
+/**
+ * Sample notification that will be triggered via the notify-send utility.
+ */
 class NotifySendNotification extends Notification implements NotifySendNotificationInterface
 {
     protected $message;
     
+    /**
+     * Constructor
+     * 
+     * @param string $message
+     * @param array $parameters
+     */
     public function __construct($message, array $parameters = array())
     {
         $this->message = $message;
@@ -15,6 +24,9 @@ class NotifySendNotification extends Notification implements NotifySendNotificat
         parent::__construct($parameters);
     }
     
+    /**
+     * @inheritDoc
+     */
     public function getMessage()
     {
         return $this->message;
