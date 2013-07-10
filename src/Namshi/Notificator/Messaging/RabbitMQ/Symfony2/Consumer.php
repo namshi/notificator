0d77500e42f49e094f58cb7d253e6b2c8cb8f1ed
odino <alessandro.nadalin@gmail.com>
2013-07-10 15:27:40 +0400

    added the Sf2 rabbitmq consumer with updated namespaces

---
+++ src/Namshi/Notificator/Messaging/RabbitMQ/Symfony2/Consumer.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Namshi\Notificator\Messaging\RabbitMQ\Symfony2;
+
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use PhpAmqpLib\Message\AMQPMessage;
+use InvalidArgumentException;
+use Namshi\Notificator\NotificationInterface;
+use Namshi\Notificator\ManagerInterface;
+
+/**
+ * Base consumer class that receives AMQP messages containing notifications and
+ * dispatches them to the notification manager.
+ */
+class Consumer implements ConsumerInterface
+{
+    const ERROR_INVALID_NOTIFICATION = "The body of the AMQP must be a serialized instance of Namshi\Notificator\NotificationInterface";
+    
+    protected  $notificationManager;
+    
+    /**
+     * Constructor
+     * 
+     * @param Namshi\Notificator\ManagerInterface $notificationManager
+     */
+    public function __construct(ManagerInterface $notificationManager)
+    {
+        $this->setNotificationManager($notificationManager);
+    }
+    
+    /**
+     * Processes the $message, by first valdating it and then dispatching it to
+     * the notification manager.
+     * 
+     * @param \PhpAmqpLib\Message\AMQPMessage $message
+     * @return bool
+     */
+    public function execute(AMQPMessage $message)
+    {
+        $notification = $this->validateNotification($message->body);
+        
+        return $this->getNotificationManager()->trigger($notification);
+    }
+    
+    /**
+     * Validates a notification by unserializing it and checking it implements
+     * the NotificationInterface.
+     * 
+     * @param string $notification
+     * @return Namshi\Notificator\NotificationInterface
+     * @throws InvalidArgumentException
+     */
+    protected function validateNotification($notification)
+    {
+        $notification = @unserialize($notification);
+        
+        if ($notification instanceOf NotificationInterface) {
+            return $notification;
+        }
+        
+        throw new InvalidArgumentException(self::ERROR_INVALID_NOTIFICATION);
+    }
+    
+    /**
+     * Returns the notification manager used by this class.
+     * 
+     * @return Namshi\Notificator\ManagerInterface
+     */
+    public function getNotificationManager()
+    {
+        return $this->notificationManager;
+    }
+
+    /**
+     * Sets the notification manager for this class.
+     * 
+     * @param Namshi\Notificator\ManagerInterface $notificationManager
+     */
+    public function setNotificationManager(ManagerInterface $notificationManager)
+    {
+        $this->notificationManager = $notificationManager;
+    }
+}
