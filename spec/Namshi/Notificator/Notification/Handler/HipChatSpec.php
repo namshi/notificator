ddf94168bc0d5adc3fd2bd431b0afab892477fca
Luis Cordova <cordoval@gmail.com>
2013-08-24 04:34:17 -0500

    add spec for handle hipchat notification

--- spec/Namshi/Notificator/Notification/Handler/HipChatSpec.php
+++ spec/Namshi/Notificator/Notification/Handler/HipChatSpec.php
@@ -5,6 +5,7 @@ namespace spec\Namshi\Notificator\Notification\Handler;
 use Namshi\Notificator\Notification\Email\EmailNotification;
 use Namshi\Notificator\Notification\HipChat\HipChatNotification;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 
 class HipChatSpec extends ObjectBehavior
 {
@@ -32,5 +33,28 @@ class HipChatSpec extends ObjectBehavior
         if ($this->getWrappedObject()->shouldHandle($otherNotification)) {
             throw new \Exception('fails');
         }
+    }
+
+    /**
+     * @param \Namshi\Notificator\Notification\HipChat\HipChatNotification $hipChatNotification
+     * @param \HipChat\HipChat $hipChatClient
+     */
+    function it_handles_hipchat_notifications($hipChatNotification, $hipChatClient)
+    {
+        $hipChatNotification->getParameter(Argument::any())->willReturn('something');
+        $hipChatNotification->getHipChatRoom()->willReturn('something');
+        $hipChatNotification->getHipChatSenderId()->willReturn('someId');
+        $hipChatNotification->getMessage()->willReturn('something');
+
+        $hipChatClient->message_room(
+            'something',
+            'someId',
+            'something',
+            'something',
+            'something',
+            'something'
+        )->shouldBeCalled();
+
+        $this->handle($hipChatNotification);
     }
 }
